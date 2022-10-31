a60557e9eba215d13d367b3e2b5e4369d6bb0f4e
meltemyigit <meltemyigit@gmail.com>
2022-10-31 15:46:17 +0100

    new

--- src/day24_CustomMethod_Return/BreakfastTasks.java
+++ src/day24_CustomMethod_Return/BreakfastTasks.java
@@ -4,6 +4,7 @@ public class BreakfastTasks {
     public static void main(String[] args) {
         initials("Meltem", "Yigit");
         initial2("cydeo","school");
+        domain("meltemyigitt@gmail.com");
     }
 
     //Create a method that can display initials of the person.
@@ -16,6 +17,12 @@ public class BreakfastTasks {
        String initials2 = firstName.charAt(0)+ "." +lastName.charAt(0);
        initials2 = initials2.toUpperCase();
        System.out.println(initials2);
+    }
+
+    //Create a method that can display the domain of the email.
+    public static void domain(String email){
+        String domain = email.substring(email.indexOf("@")+1 , email.lastIndexOf("."));
+        System.out.println("domain = " + domain);
     }
 
 
