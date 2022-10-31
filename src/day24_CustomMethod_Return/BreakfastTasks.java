097de4602a66cb3ebd484429aa40cedda5745b89
meltemyigit <meltemyigit@gmail.com>
2022-10-31 17:20:40 +0100

    new

--- src/day24_CustomMethod_Return/BreakfastTasks.java
+++ src/day24_CustomMethod_Return/BreakfastTasks.java
@@ -5,6 +5,7 @@ public class BreakfastTasks {
         initials("Meltem", "Yigit");
         initial2("cydeo","school");
         domain("meltemyigitt@gmail.com");
+        nameOfMonth(18);
     }
 
     //Create a method that can display initials of the person.
@@ -24,6 +25,20 @@ public class BreakfastTasks {
         String domain = email.substring(email.indexOf("@")+1 , email.lastIndexOf("."));
         System.out.println("domain = " + domain);
     }
+    //Create a method that can display the name of the month based on the given number to the method
+    
+    public static void nameOfMonth(int number){
+        String monthName = "";
+        if (number >=1 && number<13) {
 
+            monthName = (number==1)?"Jan":(number==2)?"Feb":(number==3)?"Mar":(number==4)?"Apr":(number==5)?"May":(number==6)?"June":
+                    (number==7)?"July":(number==8)?"Aug":(number==9)?"Sep":(number==10)?"Oct":(number==11)?"Nov":"Dec";
+            System.out.println("monthName = " + monthName);
+        }
+        else {
+            System.out.println("invalid");
+        }
+
+    }
 
 }
